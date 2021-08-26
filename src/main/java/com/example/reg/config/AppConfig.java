d3625b575fe833fa8524378b78a837a4c6b33f8f
Dori <58322578+dorikoak@users.noreply.github.com>
2021-08-26 15:20:44 +0900

    add keystore

--- src/main/java/com/example/reg/config/AppConfig.java
+++ src/main/java/com/example/reg/config/AppConfig.java
@@ -1,34 +1,34 @@
-package com.example.reg.config;
-
-import java.util.Properties;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
-import org.springframework.web.servlet.view.BeanNameViewResolver;
-
-@Configuration
-public class AppConfig {
-
-    @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        return new CommonsMultipartResolver();
-    }
-
-    @Bean
-    public BeanNameViewResolver beanNameViewResolver() {
-        BeanNameViewResolver beanNameView = new BeanNameViewResolver();
-        beanNameView.setOrder(1);
-        return beanNameView;
-    }
-
-    @Bean
-    public SimpleMappingExceptionResolver exceptionResolver() {
-        SimpleMappingExceptionResolver smer = new SimpleMappingExceptionResolver();
-        Properties pro = new Properties();
-        pro.put("Exception", "error/errorView");
-        smer.setExceptionMappings(pro);
-        return smer;
-    }
-}
+//package com.example.reg.config;
+//
+//import java.util.Properties;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+//import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+//import org.springframework.web.servlet.view.BeanNameViewResolver;
+//
+//@Configuration
+//public class AppConfig {
+//
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver() {
+//        return new CommonsMultipartResolver();
+//    }
+//
+//    @Bean
+//    public BeanNameViewResolver beanNameViewResolver() {
+//        BeanNameViewResolver beanNameView = new BeanNameViewResolver();
+//        beanNameView.setOrder(1);
+//        return beanNameView;
+//    }
+//
+//    @Bean
+//    public SimpleMappingExceptionResolver exceptionResolver() {
+//        SimpleMappingExceptionResolver smer = new SimpleMappingExceptionResolver();
+//        Properties pro = new Properties();
+//        pro.put("Exception", "error/errorView");
+//        smer.setExceptionMappings(pro);
+//        return smer;
+//    }
+//}
